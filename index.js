f77e2ff946edb3069dfb1a271be18c0e05756b58
Irene <irenemanningm@gmail.com>
2021-11-01 17:25:57 -0500

    commit tree 2044e6aa688f178b493574fff8c1f6d643ab4bb7

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function saturdayFun(activity) {
+    switch (activity) {
+        case 'bathe my dog':
+            return "This Saturday, I want to bathe my dog!";
+            break;
+        default:
+            return "This Saturday, I want to roller-skate!";
+    }
+}
+
+function mondayWork(activity) {
+    switch (activity) {
+        case 'work from home':
+            return "This Monday, I will work from home.";
+            break;
+        default: 
+           return "This Monday, I will go to the office."; 
+    }
+} 
+
+function wrapAdjective(style="*") {
+    function innerFunction(argument = 'special') {
+        return `You are ${style}${argument}${style}!`;
+    };
+    return innerFunction;
+}
